cdddb47edbf20c2d472ac23868038a497996c2b2
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2022-08-04 11:18:27 +0530

    Using Counter

--- ValidAnagram_Counter.py
+++ ValidAnagram_Counter.py
@@ -1,4 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return Counter(s) == Counter(t)
-#Counter is a sub-class that is used to count hashable objects. It implicitly creates a hash table of an iterable when invoked.
+#Counter is a sub-class that is used to count hashable objects. It implicitly creates a hash table of an iterable when invoked. 
