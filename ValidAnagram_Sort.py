43477729f0e593f8eb2d963391d8c899ae313808
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2022-08-04 11:19:40 +0530

    Using Sorting

--- ValidAnagram_Sort.py
+++ ValidAnagram_Sort.py
@@ -1,4 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
-        #If the sorted strings of s and t are equal its an anagram
+        #If the sorted strings of s and t are equal its an anagram 
